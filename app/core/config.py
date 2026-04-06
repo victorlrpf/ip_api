08aa5cdd312745c6aa49f64a9540e73612f5ca9f
Victor Ferreira <aulinovictor@gmail.com>
2026-04-06 20:01:38 -0300

    feat: adicioneo a class que realiza a configuracao com o mongo

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    app_name: str = "IP API"
+    app_token: str
+    mongodb_url: str
+    mongodb_db: str = "ip_tracker"
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+settings = Settings()
