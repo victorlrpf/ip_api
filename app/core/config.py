13f351f2cff2c459b9dd8b7bc554403df2a46f91
Victor <113861450+victorlrpf@users.noreply.github.com>
2026-04-07 10:05:25 -0300

    feat: adicionando o redis

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     mongodb_url: str
     mongodb_db: str = "ip_tracker"
     ipwhois_base_url: str = "https://ipwhois.is"
+    redis_url: str = "redis://localhost:6379/0"
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
