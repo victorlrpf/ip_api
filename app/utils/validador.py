bfb203b589545f7baf8270d2022de77732739919
Victor <113861450+victorlrpf@users.noreply.github.com>
2026-04-07 10:01:33 -0300

    feat: adicionei a validação de ip
    Added a new function to validate filter IP addresses and improved error handling in the existing IP validation function.

--- app/utils/validador.py
+++ app/utils/validador.py
@@ -1,9 +1,22 @@
 import ipaddress
+import re
 
 
 def validate_ip(ip: str) -> str:
     try:
-        validated_ip = ipaddress.ip_address(ip)
-        return str(validated_ip)
-    except ValueError:
-        raise ValueError("IP inválido.")
+        return str(ipaddress.ip_address(ip))
+    except ValueError as exc:
+        raise ValueError("IP inválido.") from exc
+
+
+def validate_filter_ip(filter_ip: str) -> str:
+    if not filter_ip:
+        return filter_ip
+
+    if len(filter_ip) < 3 or len(filter_ip) > 12:
+        raise ValueError("filter_ip deve ter entre 3 e 12 caracteres.")
+
+    if not re.match(r"^[0-9a-fA-F:.]+$", filter_ip):
+        raise ValueError("filter_ip contém caracteres inválidos.")
+
+    return filter_ip
